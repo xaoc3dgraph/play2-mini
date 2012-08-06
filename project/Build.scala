6682ad3e297061991951a0d1c2f397fc4c8f6f8c
peter hausel <peter_kovac11@hotmail.com>
2012-08-06 18:49:19 -0400

    bump to 2.0.3

--- project/Build.scala
+++ project/Build.scala
@@ -3,13 +3,13 @@ import Keys._
 
 object MinimalBuild extends Build {
   
-  lazy val buildVersion =  "2.0.1"
+  lazy val buildVersion =  "2.0.3"
   
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
   
-  lazy val play =  "play" %% "play" % buildVersion 
+  lazy val play =  "play" % "play_2.9.1" % buildVersion 
 
   lazy val root = Project(id = "play-mini", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
@@ -18,6 +18,7 @@ object MinimalBuild extends Build {
                 if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "ivy-snapshots/") 
                 else                                   Some("releases"  at nexus + "ivy-releases/")
     },
+    scalaVersion := "2.9.2",
     organization := "com.typesafe",
     resolvers += repo,
     libraryDependencies += play,
